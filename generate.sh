1c9fa2424a40f00c2719de9a03d664c056abb0f5
Piotr Esden-Tempski <piotr@esden.net>
2015-04-25 15:44:25 -0700

    Made generate.sh work on Linux and OS X

--- generate.sh
+++ generate.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash -
 
 mkdir -p compact
 
+SED='sed'
+
+[[ $(uname) == "Darwin" ]] && SED='gsed'
+
 for i in *.txt ; do
-	cat $i | gsed -f compact.sed > compact/$i
+	cat $i | ${SED} -f compact.sed > compact/$i
 done
